a8876f83abc8b3b7b3710adaa0116706b9147194
MaxWeb3r <matheus.mariano.souza@gmail.com>
2023-03-17 11:03:16 -0300

    Navegacao entra as abas com url_for + estilos

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request, url_for
 
 
 app = Flask(__name__)
@@ -6,15 +6,36 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route("/login/", methods=["GET", "POST"])
-def hello_world():
+def login():
 
     if request.method == "GET":
         return render_template("index.html", error=False)
 
     #if request.form["emailInput"] != "admin" or request.form["passwordInput"] != "secret":
-    if request.method == "POST":
-        print(request.form["emailInput"], request.form["passwordInput"])
+    if request.form['email'] != "matheus@gmail.com" and request.form['password'] != "senha":
+        return render_template("index.html", error=True)
+    else: 
+        return redirect(url_for('main'))
 
-    return render_template("index.html")
+    #return redirect(url_for('login'))
+
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    if request.method == "GET":
+        print(request.json)
+
+    return render_template("mainpage.html")
+
+@app.route("/disciplinas", methods=["GET", "POST"])
+def disciplinas():
+    return render_template("disciplinaspage.html")
+
+@app.route("/notas", methods=["GET", "POST"])
+def notas():
+    return render_template("notaspage.html")
+
+@app.route("/perfil", methods=["GET", "POST"])
+def perfil():
+    return render_template("notaspage.html")
 
 app.run()
